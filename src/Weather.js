896f232d92a6df19f702e8a4bd4dd9e27ea8974e
DESKTOP-Q3CPF03\Ville <villeryhanen11@gmail.com>
2021-09-28 19:27:28 +0300

    styles

--- src/Weather.js
+++ src/Weather.js
@@ -2,15 +2,23 @@ import React, { useState, useEffect } from "react"
 import { makeStyles } from "@material-ui/core/styles"
 import { getForeCast } from "./request"
 import { formatTime } from "./utils/formatTime"
+import WeatherCard from "./WeatherCard"
 
 const useStyles = makeStyles(() => ({
-
+	center: {
+	  display: "flex",
+	  justifyContent: "center",
+	  alignItems: "center"
+	},
+	biggerFont: {
+		fontSize: "5vw"
+	}
 }))
 
 const Weather = () => {
 	const [forecasts, setForecast] = useState([])	
 	const [focusedForecastDT, setFocusedForecastDT] = useState()
-
+	const classes = useStyles()
 	useEffect(() => {
 		getForeCast().then((response) => {
 			setForecast(response);
@@ -22,18 +30,31 @@ const Weather = () => {
 	if (!forecasts || !focusedForecastDT) return null;
 
 	const selectedForecast = forecasts.list.find(f => f.dt === focusedForecastDT)
-	return (
-		<div>
+
+	const renderWeatherItem = (forecast) => {
+		return (
 			<div>
+				<WeatherCard forecast={forecast} />
+			</div>
+		)
+	}
+	return (
+	  <div>	
+		<div>
+			<div className={`${classes.center} ${classes.biggerFont}`}>
 		      {forecasts.city.name}
 			</div>
-			<div>
-			  Aurinko nousee kello: {formatTime(forecasts.city.sunrise)}
+			<div className={classes.center}>
+			  Aurinko nousee klo: {formatTime(forecasts.city.sunrise)}
 			</div>
-			<div>
+			<div className={classes.center}>
 			  Aurinko laskee klo: {formatTime(forecasts.city.sunset)}
 			</div>
 		</div>
+		<div>
+		  {forecasts.list.map(forecast => renderWeatherItem(forecast))}
+		</div>
+      </div>
 	)
 }
 
